2ed4ba6463ef440cf57c8e8dc3e7efaf29d5a4ff
Juan Martinez <mjuanb25@gmail.com>
2026-05-15 16:32:19 -0500

    Se modifican las versiones de lottie y icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("com.airbnb.android:lottie-compose:6.7.1")
+    implementation(libs.lottie.compose)
+    implementation(libs.material.icons.extended)
 }
